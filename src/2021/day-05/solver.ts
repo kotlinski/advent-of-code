2e72b1cd9c7fd52d846d71a848b35f0c908fc761
Simon Kotlinski <simon.kotlinski@gmail.com>
2021-12-05 23:31:11 +0100

    feature(day5): implement part two

--- src/2021/day-05/solver.ts
+++ src/2021/day-05/solver.ts
@@ -24,19 +24,34 @@ export class Vector {
 
   isVertical = (): boolean => this.points[0].x === this.points[1].x;
 
+  isDiagonal = (): boolean => {
+    const k = (this.points[0].y - this.points[1].y) / (this.points[0].x - this.points[1].x);
+    return Math.abs(k) === 1;
+  };
+
   getPointsInVector(): Point[] {
     const points: Point[] = [];
-    const a = this.isHorizontal() ? this.points[0].y : this.points[0].x;
-    const min_max = this.isHorizontal() ? [this.points[0].x, this.points[1].x] : [this.points[0].y, this.points[1].y];
-    const min: number = Math.min(...min_max);
-    const max: number = Math.max(...min_max);
-    for (let b = min; b <= max; b++) {
-      const coordinate = [a, b];
-      if (this.isVertical()) coordinate.reverse();
-      const point = new Point(coordinate);
-      points.push(point);
+    const range_x = this.getRange([this.points[0].x, this.points[1].x]);
+    const range_y = this.getRange([this.points[0].y, this.points[1].y]);
+    if (this.isHorizontal()) {
+      range_x.forEach((x: number) => {
+        points.push(new Point([x, this.points[0].y]));
+      });
+    } else if (this.isVertical()) {
+      range_y.forEach((y: number) => {
+        points.push(new Point([this.points[0].x, y]));
+      });
+    } else if (this.isDiagonal()) {
+      for (let i = 0; i < range_y.length; i++) {
+        points.push(new Point([range_x[i], range_y[i]]));
+      }
     }
     return points;
+  }
+
+  private getRange([p1, p2]: number[]) {
+    const map_function = (_v: any, k: number) => (p1 < p2 ? k + Math.min(p1, p2) : Math.max(p1, p2) - k);
+    return Array.from({ length: Math.abs(p1 - p2) + 1 }, map_function);
   }
 }
 
@@ -54,16 +69,41 @@ export default class HydrothermalVentureSolver extends Solver<Vector[]> {
 
   solvePartOne(): number {
     const hydrothermal_vents = this.input.filter((vent) => vent.isHorizontal() || vent.isVertical());
+    console.log(`hydrothermal_vents: ${JSON.stringify(hydrothermal_vents.length, null, 2)}`);
+    const diagram = this.getDiagram(hydrothermal_vents);
+    HydrothermalVentureSolver.visualizeDiagram(diagram);
+    return HydrothermalVentureSolver.countOverlappingPoints(diagram);
+  }
+  solvePartTwo(): number {
+    const hydrothermal_vents = this.input.filter((vent) => vent.isHorizontal() || vent.isVertical() || vent.isDiagonal());
+    console.log(`hydrothermal_vents.length: ${JSON.stringify(hydrothermal_vents.length, null, 2)}`);
+    const diagram = this.getDiagram(hydrothermal_vents);
+    HydrothermalVentureSolver.visualizeDiagram(diagram);
+    return HydrothermalVentureSolver.countOverlappingPoints(diagram);
+  }
+
+  private static visualizeDiagram(diagram: Map<string, number>) {
+    let visual = '';
+    for (let y = 0; y <= 9; y++) {
+      for (let x = 0; x <= 9; x++) {
+        visual += diagram.get(`${x},${y}`) ?? '.';
+      }
+      visual += '\n';
+    }
+    console.log(visual);
+  }
+
+  private static countOverlappingPoints(diagram: Map<string, number>) {
+    return [...diagram.values()].filter((number) => number >= 2).length;
+  }
+
+  private getDiagram(hydrothermal_vents: Vector[]) {
     const diagram = new Map<string, number>();
     for (const vent of hydrothermal_vents) {
       vent.getPointsInVector().forEach((point: Point) => {
         diagram.set(point.hash(), 1 + (diagram.get(point.hash()) ?? 0));
       });
     }
-    return [...diagram.values()].filter((number) => number >= 2).length;
-  }
-
-  solvePartTwo(): number {
-    return 4711;
+    return diagram;
   }
 }
