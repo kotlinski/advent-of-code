286edb2373a79c6e28ddc02529b4af0051b0a1bf
Simon Kotlinski <simon.kotlinski@gmail.com>
2021-12-11 15:17:17 +0100

    feature(day8): implement task 1 (#15)

--- README.md
+++ README.md
@@ -11,6 +11,7 @@ Advent of code: https://adventofcode.com/
 - [Day 5](https://github.com/kotlinski/advent-of-code/tree/main/src/2021/day-05) 🌟 🌟
 - [Day 6](https://github.com/kotlinski/advent-of-code/tree/main/src/2021/day-06) 🌟 🌟
 - [Day 7](https://github.com/kotlinski/advent-of-code/tree/main/src/2021/day-07) 🌟 🌟
+- [Day 8](https://github.com/kotlinski/advent-of-code/tree/main/src/2021/day-08) 🌟
 
 ## How to run
 
---
+++ src/2021/day-08/solver.ts
@@ -0,0 +1,42 @@
+import Solver from '../../solver';
+
+export default class SevenSegmentSearchSolver extends Solver<string[][]> {
+  constructor(raw_input: string) {
+    super(raw_input);
+  }
+
+  parse(raw_input: string): string[][] {
+    const notes = raw_input.split('\n');
+    return notes
+      .map((note) =>
+        note
+          ?.split('|')[1]
+          ?.split(' ')
+          .filter((number) => number?.length > 0),
+      )
+      .filter((numbers) => numbers?.length > 0);
+  }
+
+  solvePartOne(): number {
+    const output_notes = this.input;
+    let count = 0;
+    output_notes.forEach((output_note) => {
+      output_note.forEach((output_value) => {
+        switch (output_value.length) {
+          case 2:
+          case 3:
+          case 4:
+          case 7:
+            count++;
+            break;
+        }
+      });
+    });
+
+    return count;
+  }
+
+  solvePartTwo(): number {
+    return 4711;
+  }
+}
