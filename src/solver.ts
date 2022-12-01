7e555d474c7523717ce4da02b84208eaae73dafd
Simon Kotlinski <simon.kotlinski@gmail.com>
2022-12-01 21:25:06 +0100

    chore(dependencies): update pakcages (#24)

--- src/solver.ts
+++ src/solver.ts
@@ -5,14 +5,18 @@
  */
 export default abstract class Solver<T> {
   protected input: T;
+
   protected constructor(raw_input: string) {
     this.input = this.parse(raw_input);
   }
+
   protected abstract parse(raw_input: string): T;
+
   public abstract solvePartOne(optional_param?: { input?: T; iterations?: number }): number;
+
   public abstract solvePartTwo(): number;
 }
 
-export function solverFactory(constructor: new (input: string) => Solver<any>, input: string): Solver<any> {
-  return new constructor(input);
+export function solverFactory(constructor: new (input: string) => Solver<any>, in_data: string): Solver<any> {
+  return new constructor(in_data);
 }
