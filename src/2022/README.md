a019da62148260ba86f8796ef53880adeb0226c9
Simon Kotlinski <simon.kotlinski@gmail.com>
2022-12-04 20:04:40 +0100

    chore(day-4): solve problems

--- src/2022/README.md
+++ src/2022/README.md
@@ -5,3 +5,4 @@
 | [Day 1](https://github.com/kotlinski/advent-of-code/tree/main/src/2022/day-01) |   🌟   |   🌟   |
 | [Day 2](https://github.com/kotlinski/advent-of-code/tree/main/src/2022/day-02) |   🌟   |   🌟   |
 | [Day 3](https://github.com/kotlinski/advent-of-code/tree/main/src/2022/day-03) |   🌟   |   🌟   |
+| [Day 4](https://github.com/kotlinski/advent-of-code/tree/main/src/2022/day-04) |   🌟   |   🌟   |
---
+++ src/2022/day-04/solver.test.ts
@@ -0,0 +1,28 @@
+import Solver from '../../solver';
+import CampCleanup from './solver';
+
+describe('2022 day 4', () => {
+  let day: Solver<number[][][]>;
+  beforeEach(() => {
+    const raw_input = '2-4,6-8\n' + '2-3,4-5\n' + '5-7,7-9\n' + '2-8,3-7\n' + '6-6,4-6\n' + '2-6,4-8\n';
+    day = new CampCleanup(raw_input);
+  });
+  describe('part one', () => {
+    it('should be 0', () => {
+      expect(day.solvePartOne()).toEqual(2);
+    });
+    it('should be 1 if 8-8', () => {
+      day = new CampCleanup('8-8,8-8\n');
+      expect(day.solvePartOne()).toEqual(1);
+    });
+    it('should be 0 if not included in large range', () => {
+      day = new CampCleanup('2-3,3-100\n');
+      expect(day.solvePartOne()).toEqual(0);
+    });
+  });
+  describe('part two', () => {
+    it('should be 70', () => {
+      expect(day.solvePartTwo()).toEqual(4);
+    });
+  });
+});
