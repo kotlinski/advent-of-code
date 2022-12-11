45fa2b2199295081516f11e13a7d05864e699006
Simon Kotlinski <simon.kotlinski@gmail.com>
2022-12-11 15:35:58 +0100

    chore(day-6): part one and two solved

---
+++ src/2022/day-06/solver.test.ts
@@ -0,0 +1,20 @@
+import Solver from '../../solver';
+import TuningTrouble from './solver';
+
+describe('2022 day 6', () => {
+  let day: Solver<string[]>;
+  beforeEach(() => {
+    const raw_input = 'mjqjpqmgbljsphdztnvjfqwrcgsmlb\n';
+    day = new TuningTrouble(raw_input);
+  });
+  describe('part one', () => {
+    it('should be index 7', () => {
+      expect(day.solvePartOne()).toEqual(7);
+    });
+  });
+  describe('part two', () => {
+    it('should be 19', () => {
+      expect(day.solvePartTwo()).toEqual(19);
+    });
+  });
+});
