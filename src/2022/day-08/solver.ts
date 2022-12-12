1a776943cab56dd40c87006a71eee358bfd6c191
Simon Kotlinski <simon.kotlinski@gmail.com>
2022-12-12 18:30:42 +0100

    feature(day-8): solve part 1 and 2

---
+++ src/2022/day-08/solver.ts
@@ -0,0 +1,37 @@
+import Solver from '../../solver';
+import { removeEmptyLinesPredicate } from '../../array-operations/filter';
+import { splitStringOnChar, stringToNumber } from '../../array-operations/map';
+import { productarize } from '../../array-operations/reduce';
+import { highToLowCompareFunction } from '../../array-operations/sort';
+import { all_directions, isFullyVisibleInDirection, numberOfVisibleTreesInDirection } from './treegrid-operations';
+
+export default class TreetopTreeHouse extends Solver<number[][]> {
+  constructor(raw_input: string) {
+    super(raw_input);
+  }
+
+  parse(raw_input: string): number[][] {
+    const lines = raw_input.split('\n').filter(removeEmptyLinesPredicate);
+    return lines.map(splitStringOnChar('')).map((line) => line.map(stringToNumber));
+  }
+
+  solvePartOne(): number {
+    let count = 0;
+    for (let row = 0; row < this.input.length; row++) {
+      for (let column = 0; column < this.input[row].length; column++) {
+        count += all_directions.some(isFullyVisibleInDirection(this.input, row, column)) ? 1 : 0;
+      }
+    }
+    return count;
+  }
+
+  solvePartTwo(): number {
+    const products: number[] = [];
+    for (let row = 0; row < this.input.length; row++) {
+      for (let column = 0; column < this.input[row].length; column++) {
+        products.push(all_directions.map(numberOfVisibleTreesInDirection(this.input, row, column)).reduce(productarize, 1));
+      }
+    }
+    return products.sort(highToLowCompareFunction())[0];
+  }
+}
