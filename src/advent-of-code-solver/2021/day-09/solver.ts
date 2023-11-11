072eca047990132f6d83325a6e33f6ecf55e6ea5
Simon Kotlinski <simon.koltlinski@gmail.com>
2023-11-12 00:43:55 +0100

    chore: move solvers to new folder

---
+++ src/advent-of-code-solver/2021/day-09/solver.ts
@@ -0,0 +1,56 @@
+import { removeEmptyLinesPredicate } from '../../common-operations/array-operations/filter';
+import { stringToNumber } from '../../common-operations/array-operations/map';
+import Solver from '../../solver';
+
+function findNeighbours(heightmap: number[][], x: number, y: number) {
+  const neighbours: number[] = [];
+  [
+    [x - 1, y],
+    [x + 1, y],
+    [x, y - 1],
+    [x, y + 1],
+  ].forEach((coordinate) => {
+    if (coordinate[0] >= 0 && coordinate[0] < heightmap.length) {
+      if (coordinate[1] >= 0 && coordinate[1] < heightmap[0].length) {
+        neighbours.push(heightmap[coordinate[0]][coordinate[1]]);
+      }
+    }
+  });
+  return neighbours;
+}
+
+function isRiskHeight(value: number, neighbours: number[]) {
+  return neighbours.every((neighbour) => neighbour > value);
+}
+
+export default class SmokeBasinSolver extends Solver<number[][]> {
+  constructor(raw_input: string) {
+    super(raw_input);
+  }
+
+  parse(raw_input: string): number[][] {
+    return raw_input
+      .split('\n')
+      .filter(removeEmptyLinesPredicate)
+      .map((line) => line.split('').map(stringToNumber));
+  }
+
+  solvePartOne(): number {
+    const risk_heights: number[] = [];
+    const heightmap = this.input;
+    for (let x = 0; x < heightmap.length; x++) {
+      for (let y = 0; y < heightmap[x].length; y++) {
+        const value = heightmap[x][y];
+        const neighbours = findNeighbours(heightmap, x, y);
+        if (isRiskHeight(value, neighbours)) {
+          risk_heights.push(value);
+        }
+      }
+    }
+    return risk_heights.reduce((risk_level, height) => risk_level + height + 1, 0);
+  }
+
+  solvePartTwo(): number {
+    return 4711;
+  }
+}
---
+++ src/advent-of-code-solver/solver.ts
@@ -0,0 +1,22 @@
+/**
+ * Abstract class for solving puzzle
+ *
+ * first and second assignment of the day
+ */
+export default abstract class Solver<T> {
+  protected input: T;
+
+  protected constructor(raw_input: string) {
+    this.input = this.parse(raw_input);
+  }
+
+  protected abstract parse(raw_input: string): T;
+
+  public abstract solvePartOne(optional_param?: { input?: T; iterations?: number }): number | string;
+
+  public abstract solvePartTwo(): number | string;
+}
+
+export function solverFactory(constructor: new (input: string) => Solver<any>, in_data: string): Solver<any> {
+  return new constructor(in_data);
+}
