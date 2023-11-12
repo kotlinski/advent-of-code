0482cec407d6749d426a3f0f844cd7d94fb446c7
kotlinski <simon.kotlinski@gmail.com>
2023-11-12 22:05:48 +0100

    feature(2020-01): solved part 2

--- src/advent-of-code-solver/2020/day-01/solver.ts
+++ src/advent-of-code-solver/2020/day-01/solver.ts
@@ -26,6 +26,19 @@ export default class ReportRepairSolver extends Solver<number[]> {
   }
 
   solvePartTwo(): number {
-    return 4711;
+    const expense_report = this.input;
+    for (let i = 0; i < expense_report.length; i++) {
+      for (let j = i; j < expense_report.length; j++) {
+        for (let k = j; k < expense_report.length; k++) {
+          const expense_a = expense_report[i];
+          const expense_b = expense_report[j];
+          const expense_c = expense_report[k];
+          if (expense_b + expense_a + expense_c === 2020) {
+            return expense_b * expense_a * expense_c;
+          }
+        }
+      }
+    }
+    throw new Error('no expense');
   }
 }
