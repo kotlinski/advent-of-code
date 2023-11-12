942fc245d9db39768f07a7b89e6ec8d31c58d459
Simon Kotlinski <simon.koltlinski@gmail.com>
2023-11-12 21:50:26 +0100

    feature(2020-01): solved part 1

---
+++ src/advent-of-code-solver/2020/day-01/__tests__/solver.test.ts
@@ -0,0 +1,20 @@
+import Solver from '../../../../advent-of-code-solver/solver';
+import ReportRepairSolver from '../solver';
+
+describe('day 1', () => {
+  let solver: Solver<number[]>;
+  beforeEach(() => {
+    const raw_input = '1721\n' + '979\n' + '366\n' + '299\n' + '675\n' + '1456\n';
+    solver = new ReportRepairSolver(raw_input);
+  });
+  describe('part one', () => {
+    it('should be 514579', () => {
+      expect(solver.solvePartOne()).toEqual(514579);
+    });
+  });
+  describe('part two', () => {
+    it('should be 4711', () => {
+      expect(solver.solvePartTwo()).toEqual(4711);
+    });
+  });
+});
