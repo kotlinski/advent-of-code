7718aef68dbdfe705dc0fb8d89cf736206087d58
Simon Kotlinski <simon.kotlinski@gmail.com>
2023-12-04 12:44:14 +0100

    feature(2023-04): solve part 2

--- src/advent-of-code-solver/2023/day-04/__tests__/solver.test.ts
+++ src/advent-of-code-solver/2023/day-04/__tests__/solver.test.ts
@@ -34,8 +34,16 @@ describe('day 4', () => {
   describe('part two', () => {
     type TestCase = { input: string; output: number };
     const cases: TestCase[] = [
-      { input: ')\n', output: 4711 },
-      { input: '()())\n', output: 4711 },
+      {
+        input:
+          'Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53\n' +
+          'Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19\n' +
+          'Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1\n' +
+          'Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83\n' +
+          'Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36\n' +
+          'Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11\n',
+        output: 30,
+      },
     ];
     describe.each(cases)('with input $input', ({ input, output }: TestCase) => {
       it(`should equal to ${output}`, () => {
--- src/advent-of-code-solver/2023/day-04/solver.ts
+++ src/advent-of-code-solver/2023/day-04/solver.ts
@@ -6,13 +6,15 @@ import { summarize } from '../../common/array-operations/reduce';
 export class ScratchCard {
   private readonly winning_numbers: number[];
   private readonly scratch_card_numbers: number[];
+  public readonly card_id: number;
+  public readonly correct_numbers: number[];
   constructor(scratch_card: string) {
+    // Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+    this.card_id = Number(scratch_card.split(':')[0].replace('Card ', '')) - 1;
     const numbers = scratch_card.split(':')[1];
     this.winning_numbers = numbers.split('|')[0].split(' ').filter(removeEmptyLinesPredicate).map(stringToNumber);
     this.scratch_card_numbers = numbers.split('|')[1].split(' ').filter(removeEmptyLinesPredicate).map(stringToNumber);
-  }
-  public getCorrectNumbers(): number[] {
-    return this.scratch_card_numbers.filter((number) => this.winning_numbers.includes(number));
+    this.correct_numbers = this.scratch_card_numbers.filter((number) => this.winning_numbers.includes(number));
   }
 }
 
@@ -30,13 +32,31 @@ export default class ScratchcardsSolver extends Solver<ScratchCard[]> {
 
   solvePartOne(): number {
     const points: number[] = this.input.map((scratch_card) => {
-      const length = scratch_card.getCorrectNumbers().length;
-      return length > 0 ? Math.pow(2, scratch_card.getCorrectNumbers().length - 1) : 0;
+      const length = scratch_card.correct_numbers.length;
+      return length > 0 ? Math.pow(2, scratch_card.correct_numbers.length - 1) : 0;
     });
     return points.reduce(summarize, 0);
   }
 
   solvePartTwo(): number {
-    return 4711;
+    return this.scratch(this.input);
+  }
+  private scratch(scratch_cards: ScratchCard[]): number {
+    const scratched_map = new Map<number, number>();
+
+    // initiate the original cards
+    scratch_cards.forEach((card) => {
+      scratched_map.set(card.card_id, 1);
+    });
+    scratch_cards.forEach((current_card) => {
+      const current_card_count = scratched_map.get(current_card.card_id) ?? 0;
+      current_card.correct_numbers.forEach((_correct_number, index) => {
+        const next_card_index = current_card.card_id + 1 + index;
+        const next_card_count = scratched_map.get(next_card_index) ?? 0;
+        scratched_map.set(next_card_index, next_card_count + current_card_count);
+      });
+    });
+
+    return [...scratched_map.values()].reduce(summarize, 0);
   }
 }
