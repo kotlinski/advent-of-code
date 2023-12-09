d77e0450db2addea27686f28b5576b4690129979
Simon Kotlinski <simon.kotlinski@gmail.com>
2023-12-09 22:15:51 +0100

    feature(2023-09): solve part 1

---
+++ src/advent-of-code-solver/2023/day-09/README.md
@@ -0,0 +1,3 @@
+<h2>--- Day 9: Mirage Maintenance ---</h2>
+
+https://adventofcode.com/2023/day/9
---
+++ src/advent-of-code-solver/2023/day-09/solver.ts
@@ -0,0 +1,21 @@
+import { OasisAndSandInstabilitySensor } from './oasis-and-sand-instability-sensor';
+import Solver from '../../../advent-of-code-solver/solver';
+
+export default class MirageMaintenanceSolver extends Solver<string> {
+  constructor(raw_input: string) {
+    super(raw_input);
+  }
+
+  parse(raw_input: string): string {
+    return raw_input;
+  }
+
+  solvePartOne(): number {
+    const sensor = new OasisAndSandInstabilitySensor(this.input);
+    return sensor.values.reduce((sum: number, value_array: number[]) => sum + sensor.extrapolate(value_array), 0);
+  }
+
+  solvePartTwo(): number {
+    return 4711;
+  }
+}
