386fa7c2132f92a312f212860607cebb44aabded
Simon Kotlinski <simon.kotlinski@gmail.com>
2023-12-20 16:52:23 +0100

    chore(2021-day6): replace cahce with memoize

--- src/advent-of-code-solver/common/cache.ts
+++ src/advent-of-code-solver/common/cache.ts
@@ -1,1 +1,11 @@
 export const cache = new Map<string, number>();
+/**
+ * See year 2023 day 12 for usage
+ */
+export function memoize<T extends unknown[], U>(fn: (...a: T) => U, resolver: (input: T) => string) {
+  const memo: Map<string, U> = new Map();
+  return function (...x: T) {
+    const key = resolver(x);
+    return memo.set(key, memo.has(key) ? memo.get(key)! : fn(...x)).get(key)!;
+  };
+}
