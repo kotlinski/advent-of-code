85346936f5cfd07aec9270c0373661c90f08dbec
Simon Kotlinski <simon.kotlinski@gmail.com>
2024-12-08 01:51:54 +0100

    day 7: solve part 1

--- src/advent-of-code-solver/2024/day-07/__tests__/solver.test.ts
+++ src/advent-of-code-solver/2024/day-07/__tests__/solver.test.ts
@@ -1,30 +1,75 @@
-import Solver from '../../../../advent-of-code-solver/solver.js';
 import BridgeRepairSolver from '../solver.js';
 import { describe, it } from 'node:test';
 import { expect } from 'expect';
 
-const input = '\n';
+const input =
+  '190: 10 19\n' +
+  '3267: 81 40 27\n' +
+  '83: 17 5\n' +
+  '156: 15 6\n' +
+  '7290: 6 8 6 15\n' +
+  '161011: 16 10 13\n' +
+  '192: 17 8 14\n' +
+  '21037: 9 7 18 13\n' +
+  '292: 11 6 16 20\n';
 
 describe('day 7', () => {
-  let solver: Solver<number[]>;
+  let solver: BridgeRepairSolver;
   describe('parser', () => {
     it('should parse the input', () => {
       solver = new BridgeRepairSolver(input);
-      expect(solver.input).toEqual([]);
+      expect(solver.input).toEqual([
+        {
+          numbers: [10, 19],
+          result: 190,
+        },
+        {
+          numbers: [81, 40, 27],
+          result: 3267,
+        },
+        {
+          numbers: [17, 5],
+          result: 83,
+        },
+        {
+          numbers: [15, 6],
+          result: 156,
+        },
+        {
+          numbers: [6, 8, 6, 15],
+          result: 7290,
+        },
+        {
+          numbers: [16, 10, 13],
+          result: 161011,
+        },
+        {
+          numbers: [17, 8, 14],
+          result: 192,
+        },
+        {
+          numbers: [9, 7, 18, 13],
+          result: 21037,
+        },
+        {
+          numbers: [11, 6, 16, 20],
+          result: 292,
+        },
+      ]);
     });
   });
   describe('part one', () => {
-    it(`should equal to 43`, () => {
+    it(`should equal to 3749`, () => {
       solver = new BridgeRepairSolver(input);
       const result = solver.solvePartOne();
-      expect(result).toEqual(43);
+      expect(result).toEqual(3749);
     });
   });
   describe('part two', () => {
-    it(`should equal to 4711`, () => {
+    it(`should equal to 11387`, () => {
       solver = new BridgeRepairSolver(input);
       const result = solver.solvePartTwo();
-      expect(result).toEqual(4711);
+      expect(result).toEqual(11387);
     });
   });
 });
--- src/advent-of-code-solver/2024/day-07/solver.ts
+++ src/advent-of-code-solver/2024/day-07/solver.ts
@@ -1,20 +1,47 @@
 import Solver from '../../../advent-of-code-solver/solver.js';
 import { removeEmptyLinesPredicate } from '../../common/array-operations/filter.js';
+import { summarize } from '../../common/array-operations/reduce.js';
 
-export default class BridgeRepairSolver extends Solver<number[]> {
+export type BridgeInput = {
+  result: number;
+  numbers: number[];
+};
+
+export default class BridgeRepairSolver extends Solver<BridgeInput[]> {
   constructor(raw_input: string) {
     super(raw_input);
   }
 
-  parse(raw_input: string): number[] {
-    return raw_input.split('\n').filter(removeEmptyLinesPredicate).map(Number);
+  parse(raw_input: string): BridgeInput[] {
+    const rows = raw_input.split('\n').filter(removeEmptyLinesPredicate);
+    return rows.map((row) => {
+      const [result, numbers] = row.split(': ');
+      return {
+        result: parseInt(result),
+        numbers: numbers.split(' ').map((number) => parseInt(number)),
+      };
+    });
   }
 
   solvePartOne(): number {
-    return 47;
+    const correct_results = this.input.filter(({ numbers, result }) => {
+      const possible_results: number[] = recursiveMath(numbers.reverse(), result);
+      return possible_results.includes(result);
+    });
+    return correct_results.map(({ result }) => result).reduce(summarize, 0);
   }
 
   solvePartTwo(): number {
     return 4711;
   }
+}
+function recursiveMath(numbers: number[], cap: number): number[] {
+  if (numbers.length === 1) return numbers;
+  const [first, ...rest] = numbers;
+  const sub_results = recursiveMath(rest, cap).filter((result) => result <= cap);
+  const enriched = sub_results.map((sub_result) => {
+    return [first + sub_result, first * sub_result];
+  });
+  const flat = enriched.flat();
+  return flat;
 }
