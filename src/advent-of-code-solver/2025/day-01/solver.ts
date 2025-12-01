bcf65292584bb8d023052298267f54048c94db95
Simon Kotlinski <simon.kotlinski@gmail.com>
2025-12-01 17:50:58 +0100

    feat(2025-1): solve part 1

--- src/advent-of-code-solver/2025/day-01/solver.ts
+++ src/advent-of-code-solver/2025/day-01/solver.ts
@@ -9,11 +9,24 @@ export default class SecretEntranceSolver extends Solver<ParsedType> {
   }
 
   parse(raw_input: string): ParsedType {
-    return raw_input.split('\n').filter(removeEmptyLinesPredicate).map(Number);
+    return raw_input
+      .split('\n')
+      .filter(removeEmptyLinesPredicate)
+      .map((line) => {
+        const parsed = line.replace('L', '-').replaceAll('R', '');
+        return Number(parsed);
+      });
   }
 
   solvePartOne(): number {
-    return 47;
+    let count = 0;
+    this.input.reduce((value, line) => {
+      const new_value = (value + line) % 100;
+      if (new_value === 0) count++;
+      console.log(`new_value: ${JSON.stringify(new_value, null, 2)}`);
+      return new_value;
+    }, 50);
+    return count;
   }
 
   solvePartTwo(): number {
